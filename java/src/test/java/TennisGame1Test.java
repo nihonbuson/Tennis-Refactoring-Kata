14d7349906c8b81ad6953029416c86c132cf6eda
nihonbuson <nihonbuson@gmail.com>
2020-08-21 18:27:24 +0900

    準備としてプレイヤーの得点を入れるロジックを外出しする

--- java/src/test/java/TennisGame1Test.java
+++ java/src/test/java/TennisGame1Test.java
@@ -24,44 +24,32 @@ public class TennisGame1Test {
 
         @Test
         public void _1_1の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 1);
+            wonPointBy("player2", 1);
             String score = tennisGame1.getScore();
             assertEquals("Fifteen-All",score);
         }
 
         @Test
         public void _2_2の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 2);
+            wonPointBy("player2", 2);
             String score = tennisGame1.getScore();
             assertEquals("Thirty-All",score);
         }
 
         @Test
         public void _3_3の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 3);
+            wonPointBy("player2", 3);
             String score = tennisGame1.getScore();
             assertEquals("Deuce",score);
         }
 
         @Test
         public void _4_4の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 4);
+            wonPointBy("player2", 4);
             String score = tennisGame1.getScore();
             assertEquals("Deuce",score);
         }
@@ -71,57 +59,40 @@ public class TennisGame1Test {
     class _どちらかが4点以上を取って差が付いている場合{
         @Test
         public void _4_3の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 4);
+            wonPointBy("player2", 3);
             String score = tennisGame1.getScore();
             assertEquals("Advantage player1",score);
         }
 
         @Test
         public void _5_3の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 5);
+            wonPointBy("player2", 3);
             String score = tennisGame1.getScore();
             assertEquals("Win for player1",score);
         }
 
         @Test
         public void _3_4の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 3);
+            wonPointBy("player2", 4);
             String score = tennisGame1.getScore();
             assertEquals("Advantage player2",score);
         }
 
         @Test
         public void _3_5の場合() {
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player1");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
-            tennisGame1.wonPoint("player2");
+            wonPointBy("player1", 3);
+            wonPointBy("player2", 5);
             String score = tennisGame1.getScore();
             assertEquals("Win for player2",score);
         }
     }
 
+    private void wonPointBy(String playerName, int point) {
+        for(int i=0; i<point; i++){
+            tennisGame1.wonPoint(playerName);
+        }
+    }
 }
