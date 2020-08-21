fa4122d1402c7c1077a90fe4d9887bdf75ef2e98
nihonbuson <nihonbuson@gmail.com>
2020-08-21 17:44:33 +0900

    同様にFifteen-Allの場合のテストケースを追加する

--- java/src/test/java/TennisGame1Test.java
+++ java/src/test/java/TennisGame1Test.java
@@ -4,8 +4,17 @@ import static org.junit.Assert.*;
 
 public class TennisGame1Test {
     @Test
-    public void _とりあえずテスト実行() {
+    public void _0_0の場合() {
         String score = new TennisGame1("P1", "P2").getScore();
         assertEquals("Love-All",score);
+    }
+
+    @Test
+    public void _1_1の場合() {
+        TennisGame1 tennisGame1 = new TennisGame1("P1", "P2");
+        tennisGame1.wonPoint("P1");
+        tennisGame1.wonPoint("P2");
+        String score = tennisGame1.getScore();
+        assertEquals("Fifteen-All",score);
     }
 }
