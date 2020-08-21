0f4569fb6c35b02f5bc8ce398da8f5f2401e7e4c
nihonbuson <nihonbuson@gmail.com>
2020-08-21 17:42:01 +0900

    テスト実行結果をexpectedに入れる

--- java/src/test/java/TennisGame1Test.java
+++ java/src/test/java/TennisGame1Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class TennisGame1Test {
     @Test
     public void _とりあえずテスト実行() {
-        new TennisGame1("P1","P2").getScore();
-        assertEquals(1,1);
+        String score = new TennisGame1("P1", "P2").getScore();
+        assertEquals("Love-All",score);
     }
 }
